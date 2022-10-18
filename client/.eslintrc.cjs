b98a66e20118c4fb145011f147d918367d6435fa
u-sho <shouhei.uechi@gmail.com>
2022-10-18 22:20:26 +0900

    🔧 typescript-eslint の設定を強めにかけて型安全性を高める

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -1,20 +1,52 @@
 /* eslint-env node */
 require("@rushstack/eslint-patch/modern-module-resolution");
 
+/** @type {import('eslint').Linter.Config} */
 module.exports = {
   root: true,
+  ignorePatterns: [".git/*", "node_modules/*", "dist/*"],
   extends: [
     "eslint:recommended",
     "plugin:vue/vue3-recommended",
     "@vue/eslint-config-typescript/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:@typescript-eslint/strict",
   ],
+  rules: {
+    // use "@/*" instead
+    "no-restricted-imports": ["error", { patterns: ["src/*", "../*"] }],
+
+    "@typescript-eslint/consistent-type-imports": [
+      "error",
+      { prefer: "type-imports", disallowTypeAnnotations: true },
+    ],
+    "@typescript-eslint/no-unsafe-argument": "off",
+    "@typescript-eslint/no-unsafe-assignment": "off",
+  },
   overrides: [
     {
       files: ["cypress/e2e/**.{cy,spec}.ts"],
       extends: ["plugin:cypress/recommended"],
     },
+    {
+      files: ["**/__tests__/**/*.{spec,test}.ts"],
+      rules: {
+        "no-restricted-imports": ["error", { patterns: ["src/*", "../../"] }],
+      },
+    },
   ],
+  parser: "vue-eslint-parser",
   parserOptions: {
+    ecmaFeatures: { impliedStrict: true, jsx: false },
     ecmaVersion: "latest",
+    parser: "@typescript-eslint/parser",
+    project: [
+      "./tsconfig.config.json",
+      "./tsconfig.app.json",
+      "./tsconfig.vitest.json",
+      "./tsconfig.json",
+      "./tsconfig.eslint.json",
+    ],
+    vueFeatures: { filter: false },
   },
 };
