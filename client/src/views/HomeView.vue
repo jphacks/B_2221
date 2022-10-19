cbcfa9096b288a972ccce08a578751f9896077de
u-sho (Shouhei Uechi) <26079835+u-sho@users.noreply.github.com>
2022-10-19 14:31:02 +0900

    マイページ（仮） (#9)
    APIとのつなぎ部分はモックデータを使用しています。
    
    #5 のたたき台です。

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,9 +1,63 @@
 <script setup lang="ts">
-import TheWelcome from '@/components/TheWelcome.vue'
+// TODO: APIから取得する
+const myIcon = 'src/assets/logo.svg'
+const snsLists = [
+  {
+    type: 'github',
+    icon: 'src/assets/icons/GitHub-Mark-120px-plus.png',
+    userName: '@u-sho',
+    url: 'https://github.com/u-sho',
+  },
+  {
+    type: 'github',
+    icon: 'src/assets/icons/GitHub-Mark-120px-plus.png',
+    userName: '@u-sho',
+    url: 'https://github.com/u-sho',
+  },
+]
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1>マイページ</h1>
+    <img
+      :src="myIcon"
+      alt="My photo（仮）"
+      width="300"
+    />
+    <ul>
+      <li
+        v-for="sns in snsLists"
+        :key="sns.url"
+      >
+        <a :href="sns.url">
+          <img
+            :src="sns.icon"
+            :alt="sns.type + ' icon'"
+            width="24"
+          />
+          {{ sns.userName }}
+        </a>
+      </li>
+    </ul>
   </main>
 </template>
+
+<style scoped>
+main {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+ul {
+  text-align: center;
+  padding: 0 0 0 0;
+}
+li {
+  list-style: none;
+  height: 1.5rem;
+  line-height: 1;
+  font-size: 1.5rem;
+  margin: 1.5rem 0;
+}
+</style>
