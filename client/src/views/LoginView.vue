9b1557f7111a78ebca8febccc8d65b66da67b0d5
YoshiShu777 <yoshimura@domain.com>
2022-10-20 20:36:51 +0900

    ログイン画面とサインアップ画面を作成しました

---
+++ client/src/views/LoginView.vue
@@ -0,0 +1,56 @@
+
+<script setup lang="ts">
+// TODO: APIから取得する
+import { ref } from 'vue'
+const text_Id = ref('')
+const text_Pw = ref('')
+const myIcon = 'src/assets/logo.svg'
+function IdInput(e) {
+  text_Id.value = e.target.value
+}
+function PwInput(e) {
+  text_Pw.value = e.target.value
+}
+//function Login()
+//function SignUp()
+</script>
+
+<template>
+  <main>
+    <h1>ログイン</h1>
+    <img
+      :src="myIcon"
+      alt="My photo（仮）"
+      width="250"
+    />
+    <input :value="text_Id" @input="IdInput" placeholder="Your ID">
+    <input type="password" :value="text_Pw" @input="PwInput" placeholder="Your Password">
+    <button style="background: orange">Login</button>
+    <item>
+      <button>Sign up</button>
+    </item>
+  </main>
+</template>
+
+<style scoped>
+main {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+input {
+  font-size: 1rem;
+  padding: 0.8rem;
+  margin: 0.5rem;
+  width: 250px;
+  letter-spacing: 0.1rem;
+}
+button {
+  font-size: 1rem;
+  padding: 0.5rem;
+  margin: 0.5rem;
+  width: 250px;
+  border-color: white;
+  letter-spacing: 0.1rem;
+}
+</style>
---
+++ client/src/views/SignupView.vue
@@ -0,0 +1,58 @@
+
+<script setup lang="ts">
+// TODO: APIから取得する
+import { ref } from 'vue'
+const text_Id = ref('')
+const text_Pw = ref('')
+const text_Pw_2 = ref('')
+const myIcon = 'src/assets/logo.svg'
+function IdInput(e) {
+  text_Id.value = e.target.value
+}
+function PwInput(e) {
+  text_Pw.value = e.target.value
+}
+function PwInput_2(e) {
+  text_Pw_2.value = e.target.value
+}
+//function Login()
+//function SignUp()
+</script>
+
+<template>
+  <main>
+    <h1>新規登録</h1>
+    <img
+      :src="myIcon"
+      alt="My photo（仮）"
+      width="250"
+    />
+    <input :value="text_Id" @input="IdInput" placeholder="Your ID">
+    <input type="password" :value="text_Pw" @input="PwInput" placeholder="New Password">
+    <input type="password" :value="text_Pw_2" @input="PwInput_2" placeholder="Once New Password">
+    <button style="background: orange">Sign up</button>
+  </main>
+</template>
+
+<style scoped>
+main {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+input {
+  font-size: 1rem;
+  padding: 0.8rem;
+  margin: 0.5rem;
+  width: 250px;
+  letter-spacing: 0.1rem;
+}
+button {
+  font-size: 1rem;
+  padding: 0.5rem;
+  margin: 0.5rem;
+  width: 250px;
+  border-color: white;
+  letter-spacing: 0.1rem;
+}
+</style>
