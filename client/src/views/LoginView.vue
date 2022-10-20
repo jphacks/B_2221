721450a826724235f1ae9fcc23e339bfb054d3c6
YoshiShu777 <115878531+YoshiShu777@users.noreply.github.com>
2022-10-20 21:50:19 +0900

    ログイン画面とサインアップ画面を作成しました (#11)
    Co-authored-by: YoshiShu777 <yoshimura@domain.com>

---
+++ client/src/views/LoginView.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+// TODO: APIから取得する
+import { ref } from 'vue'
+const textId = ref('')
+const password = ref('')
+const myIcon = 'src/assets/logo.svg'
+//function Login()
+//function SignUp()
+</script>
+
+<template>
+  <main>
+    <h1>ログイン</h1>
+    <img
+      :src="myIcon"
+      alt="My photo（仮）"
+      width="250"
+    />
+    <input
+      v-model="textId"
+      placeholder="Your ID"
+    />
+    <input
+      v-model="password"
+      type="password"
+      placeholder="Your Password"
+    />
+    <button style="background: orange">Login</button>
+    <a href="/signup">Sign up</a>
+  </main>
+</template>
+
+<style scoped>
+main {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+input {
+  font-size: 1rem;
+  padding: 0.8rem;
+  margin: 0.5rem;
+  width: 250px;
+  letter-spacing: 0.1rem;
+}
+button {
+  font-size: 1rem;
+  padding: 0.5rem;
+  margin: 0.5rem;
+  width: 250px;
+  border-color: white;
+  letter-spacing: 0.1rem;
+}
+</style>
---
+++ client/src/views/SignupView.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+// TODO: APIから取得する
+import { ref } from 'vue'
+const textId = ref('')
+const password = ref('')
+const myIcon = 'src/assets/logo.svg'
+
+//function Login()
+//function SignUp()
+</script>
+
+<template>
+  <main>
+    <h1>新規登録</h1>
+    <img
+      :src="myIcon"
+      alt="My photo（仮）"
+      width="250"
+    />
+    <input
+      v-model="textId"
+      placeholder="Your ID"
+    />
+    <input
+      v-model="password"
+      type="password"
+      placeholder="New Password"
+    />
+    <button style="background: orange">Sign up</button>
+  </main>
+</template>
+
+<style scoped>
+main {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+input {
+  font-size: 1rem;
+  padding: 0.8rem;
+  margin: 0.5rem;
+  width: 250px;
+  letter-spacing: 0.1rem;
+}
+button {
+  font-size: 1rem;
+  padding: 0.5rem;
+  margin: 0.5rem;
+  width: 250px;
+  border-color: white;
+  letter-spacing: 0.1rem;
+}
+</style>
